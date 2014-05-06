04fd53386420f3dc5add2c5977bc54882327984b
robfrank <ro.franchini@gmail.com>
2014-05-07 00:00:44 +0200

    imperative java extractMood method implemented

--- jtfp-java/src/main/java/it/jugtofunprog/textanalysis/JavaAnalyzedTextProcessor.java
+++ jtfp-java/src/main/java/it/jugtofunprog/textanalysis/JavaAnalyzedTextProcessor.java
@@ -1,14 +1,25 @@
 package it.jugtofunprog.textanalysis;
 
 import it.jugtofunprog.textanalysis.model.AnalyzedText;
+import it.jugtofunprog.textanalysis.model.Annotation;
 import it.jugtofunprog.textanalysis.model.Mood;
+import it.jugtofunprog.textanalysis.model.Polarity;
+
+import java.util.List;
 
 public class JavaAnalyzedTextProcessor implements AnalyzedTextProcessor {
 
     @Override
     public Mood extractMood(final AnalyzedText analyzedText) {
-        // TODO: your code here!!!
-        return null;
+        List<Annotation> annotations = analyzedText.getAnnotations();
+
+        for (Annotation annotation : annotations) {
+            if (annotation instanceof Polarity) {
+                Polarity pol = (Polarity) annotation;
+                return pol.getMood();
+            }
+        }
+        return Mood.NONE;
     }
 
 
