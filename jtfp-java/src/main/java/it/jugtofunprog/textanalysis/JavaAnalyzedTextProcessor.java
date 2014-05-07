6fd7ab38f3469dd31028fd6c8e2ecac8099e89f3
robfrank <ro.franchini@gmail.com>
2014-05-08 00:14:12 +0200

    added ugliest method

--- jtfp-java/src/main/java/it/jugtofunprog/textanalysis/JavaAnalyzedTextProcessor.java
+++ jtfp-java/src/main/java/it/jugtofunprog/textanalysis/JavaAnalyzedTextProcessor.java
@@ -5,6 +5,7 @@ import it.jugtofunprog.textanalysis.model.Annotation;
 import it.jugtofunprog.textanalysis.model.Mood;
 import it.jugtofunprog.textanalysis.model.Polarity;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class JavaAnalyzedTextProcessor implements AnalyzedTextProcessor {
@@ -22,6 +23,26 @@ public class JavaAnalyzedTextProcessor implements AnalyzedTextProcessor {
         return Mood.NONE;
     }
 
+
+    public Mood extractMood_UGLIEST(final AnalyzedText analyzedText) {
+        List<Annotation> annotations = analyzedText.getAnnotations();
+        Mood mood = null;
+        boolean found = false;
+        Iterator<Annotation> it = annotations.iterator();
+        while (it.hasNext() && found == false) {
+            Annotation a = it.next();
+            if (a instanceof Polarity) {
+                mood = ((Polarity) a).getMood();
+                found = true;
+            }
+        }
+
+        if (mood == null) {
+            mood = Mood.NONE;
+        }
+
+        return mood;
+    }
 
 
 }
